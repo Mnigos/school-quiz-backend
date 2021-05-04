d6fc2376662686b1b3b61b3e7167f7de82c6eb5f
MoneyIgos <makowskiigor@gmail.com>
2021-05-04 12:30:43 +0200

    chore(app.module): create configuration for env file and mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
+import { MongooseModule } from '@nestjs/mongoose'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
