01abc51111e7ded964a127bd3c1513a278298e8b
MoneyIgos <makowskiigor@gmail.com>
2021-05-07 11:33:09 +0200

    feat(users): create constructor for users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@nestjs/common'
+import { HttpStatus, Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+import { IUser } from './interfaces/users.interface'
+import { UserDoc } from './interfaces/usersDoc.interface'
 
+import * as bcrypt from 'bcrypt'
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel('Users') private readonly UserModel: Model<UserDoc>) {}
+}
