3ca664042b3da08b30f8a876e0afe8ba8ea68918
MoneyIgos <makowskiigor@gmail.com>
2021-05-07 11:33:41 +0200

    feat(users): create endpoint for users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,4 +8,48 @@ import * as bcrypt from 'bcrypt'
 @Injectable()
 export class UsersService {
   constructor(@InjectModel('Users') private readonly UserModel: Model<UserDoc>) {}
+
+  async getAll(): Promise<IUser[]> {
+    return await this.UserModel.find().exec()
+  }
+
+  async getOneById(_id: string): Promise<IUser | HttpStatus> {
+    const user = await this.UserModel.findOne({ _id }).exec()
+
+    if (!user) return HttpStatus.BAD_REQUEST
+
+    return user
+  }
+
+  async getOneByName(name: string): Promise<IUser | HttpStatus> {
+    const user = await this.UserModel.findOne({ name }).exec()
+
+    if (!user) return HttpStatus.BAD_REQUEST
+
+    return user
+  }
+
+  async update(user: IUser): Promise<IUser | HttpStatus> {
+    const { _id } = user
+
+    if (!_id) return HttpStatus.BAD_REQUEST
+
+    this.UserModel.updateOne({ _id }, user)
+
+    return await this.UserModel.findOne({ _id }).exec()
+  }
+
+  async create(user: IUser): Promise<boolean> {
+    const { name, pass } = user
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(pass, salt)
+
+    await this.UserModel.create({
+      name,
+      pass: hash,
+    } as IUser)
+
+    return true
+  }
 }
