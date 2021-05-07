58a07a1d46c6410a54a5722517e5f85e4b6dda40
MoneyIgos <makowskiigor@gmail.com>
2021-05-07 11:45:01 +0200

    feat(auth): create controller for auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common'
-import { AppService } from './app.service'
+import { Controller, Request, Post, UseGuards, Res, Get } from '@nestjs/common'
+import { AuthService } from './auth/auth.service'
+import { LocalAuthGuard } from './auth/local-auth.guard'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/register')
+  async register(@Request() req) {
+    return this.authService.register(req.body)
+  }
+
+  @Post('auth/login')
+  async login(@Request() req, @Res({ passthrough: true }) res) {
+    const { access_token } = await this.authService.login(req.body)
+
+    res.cookie('jwt', access_token, { httpOnly: true })
+
+    return { access_token }
+  }
+
+  @Get('auth/user')
+  async user(@Request() req) {
+    return { access_token: req.cookies['jwt'] }
   }
 }
