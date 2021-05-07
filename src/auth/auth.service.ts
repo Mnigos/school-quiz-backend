3e2fc888ab5a876371ea0dcd3cd2a795b2600aef
MoneyIgos <makowskiigor@gmail.com>
2021-05-07 11:42:02 +0200

    feat(auth): create service for auth endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common'
+import { HttpStatus, Injectable } from '@nestjs/common'
+import { UsersService } from '../users/users.service'
+import * as bcrypt from 'bcrypt'
+import { IUser } from '../users/interfaces/users.interface'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService, private jwtService: JwtService) {}
+
+  async validateUser(user: IUser): Promise<any> {
+    const { name, pass } = user
+    const foundedUser = await this.usersService.getOneByName(name)
+    if (
+      foundedUser !== HttpStatus.BAD_REQUEST &&
+      bcrypt.compare(pass, (foundedUser as IUser).pass)
+    ) {
+      const { _id, name } = foundedUser as IUser
+
+      return {
+        _id,
+        name,
+      }
+    }
+
+    return null
+  }
+
+  async login(user: IUser): Promise<any> {
+    const { name, _id } = user
+
+    return {
+      access_token: this.jwtService.sign({ name, sub: _id }),
+    }
+  }
+
+  async register(user: IUser): Promise<any> {
+    return await this.usersService.create(user)
+  }
+}
