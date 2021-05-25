2913892427ebd242a5044ce553c33c59ae1806a1
MoneyIgos <makowskiigor@gmail.com>
2021-05-25 13:38:54 +0200

    refactor(quiz): add _id to quiz interface

--- src/quiz/interfaces/quiz.interface.ts
+++ src/quiz/interfaces/quiz.interface.ts
@@ -2,6 +2,7 @@ import { IQuestion } from './question.interface'
 import { ITaker } from './taker.interface'
 
 export interface IQuiz {
+  _id: string
   name: string
   questions: IQuestion[]
   rightAnswers: string[]
