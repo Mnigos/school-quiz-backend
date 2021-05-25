ed541b52de086e7c3762beda46562c308c570446
MoneyIgos <makowskiigor@gmail.com>
2021-05-25 13:40:35 +0200

    refactor(quiz): change _id to be optional

--- src/quiz/interfaces/quiz.interface.ts
+++ src/quiz/interfaces/quiz.interface.ts
@@ -2,7 +2,7 @@ import { IQuestion } from './question.interface'
 import { ITaker } from './taker.interface'
 
 export interface IQuiz {
-  _id: string
+  _id?: string
   name: string
   questions: IQuestion[]
   rightAnswers: string[]
